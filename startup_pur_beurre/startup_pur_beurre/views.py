54c2070cdcdc8ae2a57828a3ce409cf5f43c43fa
thomas <psychoid@hotmail.fr>
2020-11-21 11:40:41 +0100

    creation app 'users','database' et 'display'

--- startup_pur_beurre/startup_pur_beurre/views.py
+++ startup_pur_beurre/startup_pur_beurre/views.py
@@ -4,18 +4,16 @@
 from django.shortcuts import render
 
 def home(request):
-    
     return render(request, 'pages/home.html')
 
 def results_of_research(request):
-
     return render(request, 'pages/results_of_research.html')
 
 def saved_products(request):
-    
     return render(request, 'pages/saved_products.html')
 
 def selected_product(request):
+    
     return render(request, 'pages/selected_product.html')
 
 def user_account(request):
