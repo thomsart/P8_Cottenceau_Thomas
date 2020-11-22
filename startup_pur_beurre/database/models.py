28e7bb467e517f2d276fadd925b6a0809cd3c50d
thomas <psychoid@hotmail.fr>
2020-11-22 12:08:49 +0100

    models

--- startup_pur_beurre/database/models.py
+++ startup_pur_beurre/database/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_lenght=40)
+    brand = models.CharField(max_lenght=40)
+    store = models.TextField()
+    nutriscore = models.CharField(max_lenght=1)
+    fat_lipids_100g = models.CharField(max_lenght=10)
+    saturated_fatty_acids_100g = models.CharField(max_lenght=10)
+    sugar_100g = models.CharField(max_lenght=10)
+    salt_100g = models.CharField(max_lenght=10)
+    photo = 
+
+
