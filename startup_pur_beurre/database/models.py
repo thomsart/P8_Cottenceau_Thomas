83118c215f97caa289ffb79c1788e664ac93bf6f
thomas <psychoid@hotmail.fr>
2020-11-22 13:16:39 +0100

    creation des tables/models

--- startup_pur_beurre/database/models.py
+++ startup_pur_beurre/database/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
+class User(models.Model):
+    id = models.PositiveIntegerField()
+    first_name = models.CharField(max_lenght=40)
+    last_name = models.CharField(max_lenght=40)
+    sex = models.CharField(max_lenght=1)
+    e_mail = models.EmailField()
+
+class SavedProducts(models.Model):
+    id_user = models.PositiveIntegerField()
+    id_product = models.PositiveIntegerField()
+    cat = models.CharField(max_lenght=20)
+
+class Cornflakes(models.Model):
+    id = models.PositiveIntegerField()
+    cat = models.CharField(max_lenght=20)
     name = models.CharField(max_lenght=40)
     brand = models.CharField(max_lenght=40)
     store = models.TextField()
@@ -11,6 +25,45 @@ class Product(models.Model):
     saturated_fatty_acids_100g = models.CharField(max_lenght=10)
     sugar_100g = models.CharField(max_lenght=10)
     salt_100g = models.CharField(max_lenght=10)
-    photo = 
+    photo = models.ImageField()
+
+class Spaghetti(models.Model):
+    id = models.PositiveIntegerField()
+    cat = models.CharField(max_lenght=20)
+    name = models.CharField(max_lenght=40)
+    brand = models.CharField(max_lenght=40)
+    store = models.TextField()
+    nutriscore = models.CharField(max_lenght=1)
+    fat_lipids_100g = models.CharField(max_lenght=10)
+    saturated_fatty_acids_100g = models.CharField(max_lenght=10)
+    sugar_100g = models.CharField(max_lenght=10)
+    salt_100g = models.CharField(max_lenght=10)
+    photo = models.ImageField()
+
+class Riz(models.Model):
+    id = models.PositiveIntegerField()
+    cat = models.CharField(max_lenght=20)
+    name = models.CharField(max_lenght=40)
+    brand = models.CharField(max_lenght=40)
+    store = models.TextField()
+    nutriscore = models.CharField(max_lenght=1)
+    fat_lipids_100g = models.CharField(max_lenght=10)
+    saturated_fatty_acids_100g = models.CharField(max_lenght=10)
+    sugar_100g = models.CharField(max_lenght=10)
+    salt_100g = models.CharField(max_lenght=10)
+    photo = models.ImageField()
+
+class PatteTartiner(models.Model):
+    id = models.PositiveIntegerField()
+    cat = models.CharField(max_lenght=20)
+    name = models.CharField(max_lenght=40)
+    brand = models.CharField(max_lenght=40)
+    store = models.TextField()
+    nutriscore = models.CharField(max_lenght=1)
+    fat_lipids_100g = models.CharField(max_lenght=10)
+    saturated_fatty_acids_100g = models.CharField(max_lenght=10)
+    sugar_100g = models.CharField(max_lenght=10)
+    salt_100g = models.CharField(max_lenght=10)
+    photo = models.ImageField()
 
 
