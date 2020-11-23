fb479e6767934ed298c1edcfb2bc6a6ae11d4803
thomas <psychoid@hotmail.fr>
2020-11-23 18:02:43 +0100

    db faite

--- startup_pur_beurre/database/models.py
+++ startup_pur_beurre/database/models.py
@@ -2,69 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
+class Users(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
     sex = models.CharField(max_length=1)
     e_mail = models.EmailField()
+    password = models.CharField(max_length=20)
+
+class Products(models.Model):
+    cat = models.CharField(max_length=20)
+    name = models.CharField(max_length=40)
+    brand = models.CharField(max_length=40)
+    store = models.TextField()
+    nutriscore = models.CharField(max_length=1)
+    fat_lipids_100g = models.CharField(max_length=10)
+    saturated_fatty_acids_100g = models.CharField(max_length=10)
+    sugar_100g = models.CharField(max_length=10)
+    salt_100g = models.CharField(max_length=10)
+    photo = models.ImageField()
 
 class SavedProducts(models.Model):
-    id_user = models.PositiveIntegerField()
-    cat = models.CharField(max_length=20)
-    id_product = models.PositiveIntegerField()
-    
-
-class CornFlakes(models.Model):
-
-    cat = models.CharField(max_length=20)
-    name = models.CharField(max_length=40)
-    brand = models.CharField(max_length=40)
-    store = models.TextField()
-    nutriscore = models.CharField(max_length=1)
-    fat_lipids_100g = models.CharField(max_length=10)
-    saturated_fatty_acids_100g = models.CharField(max_length=10)
-    sugar_100g = models.CharField(max_length=10)
-    salt_100g = models.CharField(max_length=10)
-    photo = models.ImageField()
-
-class Spaghetti(models.Model):
-
-    cat = models.CharField(max_length=20)
-    name = models.CharField(max_length=40)
-    brand = models.CharField(max_length=40)
-    store = models.TextField()
-    nutriscore = models.CharField(max_length=1)
-    fat_lipids_100g = models.CharField(max_length=10)
-    saturated_fatty_acids_100g = models.CharField(max_length=10)
-    sugar_100g = models.CharField(max_length=10)
-    salt_100g = models.CharField(max_length=10)
-    photo = models.ImageField()
-
-class Riz(models.Model):
-
-    cat = models.CharField(max_length=20)
-    name = models.CharField(max_length=40)
-    brand = models.CharField(max_length=40)
-    store = models.TextField()
-    nutriscore = models.CharField(max_length=1)
-    fat_lipids_100g = models.CharField(max_length=10)
-    saturated_fatty_acids_100g = models.CharField(max_length=10)
-    sugar_100g = models.CharField(max_length=10)
-    salt_100g = models.CharField(max_length=10)
-    photo = models.ImageField()
-
-class PatteTartiner(models.Model):
-
-    cat = models.CharField(max_length=20)
-    name = models.CharField(max_length=40)
-    brand = models.CharField(max_length=40)
-    store = models.TextField()
-    nutriscore = models.CharField(max_length=1)
-    fat_lipids_100g = models.CharField(max_length=10)
-    saturated_fatty_acids_100g = models.CharField(max_length=10)
-    sugar_100g = models.CharField(max_length=10)
-    salt_100g = models.CharField(max_length=10)
-    photo = models.ImageField()
-
-
+    user = models.ManyToManyField(Users, verbose_name="User ID")
+    product = models.ManyToManyField(Products, verbose_name="Product ID")
--- startup_pur_beurre/startup_pur_beurre/urls.py
+++ startup_pur_beurre/startup_pur_beurre/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('', include('display.urls')),
     path('admin/', admin.site.urls),
+    path('', include('display.urls')),
 ]
