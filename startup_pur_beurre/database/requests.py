a2e295074553d6328d593488a2532e6885015f8f
thomas <psychoid@hotmail.fr>
2020-11-27 11:05:24 +0100

    request.py fini

--- startup_pur_beurre/database/requests.py
+++ startup_pur_beurre/database/requests.py
@@ -4,13 +4,13 @@
 
 import requests
 
-from . import models
+from . import models as mdl
 
 ################################################################################
 
 def main():
     """
-    We take from Openfoodfact Api a category of 'food' we want and put it in a
+    We take from Openfoodfact Api a 'category' of food we want and put it in a
     list of values in order to fill the table made for it in our database.
     For that we create a for loop in fonction of the number of pages
     'nb_page' of the product on the Api, and we put all datas in the table
@@ -20,21 +20,14 @@ def main():
 
     category = input("Quelle categorie ?\n")
     nb_pages = int(input("Combien de page ?\n"))
-    print("Ok")
-
-    self.cursor.execute("""INSERT INTO category (name) VALUE('"""
-                        + str(category) + """') """)
-
-    self.cursor.execute("""SELECT id FROM category WHERE name = '"""
-                        + str(category) + """' """)
-
+    print("Ok c'est parti !")
 
     for i in range(nb_pages):
-
-        """ We choose the food we want to put in the database by the argument
-        'food' and the number of pages we want with 'nb_pages'. Be carefull
-        to not take more than there are pages on the Api. """
-
+        """
+        We choose the food we want to put in the database by the argument
+        'category' and the number of pages we want with 'nb_pages'. Be carefull
+        to not take more than there are pages on the Api.
+        """
         url = 'https://fr-en.openfoodfacts.org/category/' + category + '/'
         + str(i) + '.json'
         response = requests.get(url)
@@ -43,50 +36,40 @@ def main():
         number_of_product = len(key)
         count = 0
 
-        """ Now we do a while loop to browse and catch all the vallues which
-        we need for our table. """
-
         while count < number_of_product:
-
+            """
+            Now we do a while loop to browse and catch all the vallues which
+            we need for our table.
+            """
             try:
-                product = key[count]
-                name = product['product_name']
-                brand = product['brands']
-                store = product['stores']
-                country = product['countries']
-                quantity = product['quantity']
-                nutriscore = product['nutriscore_grade']
-                url = product['url']
-                category = str(category)
-                product_list = [name, brand, store, country, quantity,
-                                nutriscore, url, category]
+
+                productApi = key[count]
+                product = mdl.Products()
+
+                product.cat = category
+                product.name = productApi['product_name']
+                product.brand = productApi['brands']
+                product.store = productApi['stores']
+                product.nutriscore = productApi['nutriscore_grade']
+                product.fat_lipids_100g = productApi['fat_100g']
+                product.saturated_fatty_acids_100g = productApi['saturated-fat_100g']
+                product.sugar_100g = productApi['sugars_100g']
+                product.salt_100g = productApi['salt_100g']
+                product.photo = productApi['image_url']
+                
+                product_list = [product.cat, product.name, product.brand, product.store,
+                product.nutriscore, product.fat_lipids_100g, product.saturated_fatty_acids_100g,
+                product.sugar_100g, product.salt_100g]
                 print(product_list)
 
-                """ We choose to not take product which don't have the
-                headers that we need. """
-
-            except KeyError:
+            except Exception:
+                """
+                We choose to not take product which don't have the headers that we need.
+                """
                 count += 1
                 continue
 
             count += 1
-
-            """ Now the idea is to put the product_list into the table we
-            choose. """
-
-            self.cursor.execute("""INSERT INTO product(name, brand, store,
-                country, quantity,nutriscore, url, category)
-                VALUE(%s, %s, %s, %s, %s, %s, %s, %s)""", product_list)
-
-    self.cursor.execute("""SELECT id FROM product WHERE category =
-                        '"""+str(food)+"""' """)
-
-    the_ids = self.cursor.fetchall()
-
-    for el in the_ids:
-        self.cursor.execute("""INSERT INTO product_category (id_product,
-                            id_category) VALUES('"""+str(el[0])+"""',
-                            '"""+str(id_cat)+"""')""")
 
     return
 
