a29c264fe11e70b049cd95e62259e956471f637d
thomas <psychoid@hotmail.fr>
2020-12-14 12:27:32 +0100

    recuperation données form ok

--- startup_pur_beurre/users/form.py
+++ startup_pur_beurre/users/form.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import forms, UserCreationForm, UserChangeForm
 
 from .models import ClientUser
 
@@ -17,3 +17,16 @@ class LoginForm(UserChangeForm):
     class Meta:
         model = ClientUser
         fields = ('email', 'password')
+
+class SearchProductForm(forms.Form):
+
+    product_name = forms.CharField(label='', max_length=70)
+
+    pass
+
+
+class SearchForm(forms.Form):
+
+    search = forms.CharField(label='aucune idée', max_length=100)
+
+    pass
--- startup_pur_beurre/users/views.py
+++ startup_pur_beurre/users/views.py
@@ -3,31 +3,65 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
 
 from database.models import Products, SavedProducts
-from .form import AccountForm
+from .form import *
 
 # Create your views here.
 
 class SignUpView(CreateView):
+
     form_class = AccountForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-def home(request):
-    return render(request, 'home.html')
 
-def results_of_research(request):
+def home(request):
+
+    product_wanted = SearchProductForm(request.POST)
+    context = {'search_product': product_wanted}
+
+    if request.method == 'POST':
+        if product_wanted.is_valid():
+            product_name = product_wanted.cleaned_data['product_name']
+            print(product_name)
+
+            product_from_db = Products.objects.filter(name=product_name).values()
+
+            if product_from_db[0]['name'] != None:
+                # we send it to the page 'results_of_research'
+                print("On y est la")
+                print(product_from_db[0])
+                pass
+            else:
+                print("Ce produit n'est pas référencé")
+                # return HttpResponseRedirect('results_of_research.html') 
+                
+
+        else:
+            product_wanted = SearchProductForm()
+
+    return render(request, 'home.html', context)
+
+
+def results_of_research(request, HttpResponse):
+
+
     return render(request, 'results_of_research.html')
+
 
 def selected_product(request):
     return render(request, 'selected_product.html')
 
+
 def account(request):
     return render(request, 'account.html')
 
+
 def saved_products(request):
     return render(request, 'saved_products.html')
+
 
 def mentions_legales(request):
     return render(request, 'mentions_legales.html')
