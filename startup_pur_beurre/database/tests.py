fa6396a9fa630efa05d55744ec7929f495cd94e6
thomas <psychoid@hotmail.fr>
2021-02-02 18:15:47 +0100

    test not ok

--- startup_pur_beurre/database/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase, Products, SavedProducts
-
-# Create your tests here.
-
-product = Products()
-saved_products = SavedProducts()
--- startup_pur_beurre/users/tests/test_views.py
+++ startup_pur_beurre/users/tests/test_views.py
@@ -1,5 +1,6 @@
 from django import urls
 from django.contrib.auth import get_user_model
+from users.models import ClientUser
 import pytest
 
 
@@ -8,16 +9,25 @@ import pytest
 @pytest.mark.parametrize('param', [
     'home',
     'signup',
-    'account/',
-    'search_product/',
-    'selected_product/',
-    'proposed_products/',
-    'save_product/',
-    'user_substitutes/',
-    'delete_product/',
+    # 'account/',
+    # 'search_product/',
+    # 'selected_product/',
+    # 'proposed_products/',
+    # 'save_product/',
+    # 'user_substitutes/',
+    # 'delete_product/',
     'mentions_legales/'
 ])
 def test_render_views(client, param):
     temp_url = urls.reverse(param)
     response = client.get(temp_url)
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_signup(client, user_data):
+    user_model = get_user_model()
+    assert user_model.objects.count() == 0
+    signup_url = urls.reverse('signup')
+    response = client.post(signup_url, user_data)
+    assert user_model.objects.count() == 1
+    assert response.status_code == 302
