8145b9f5e8ceeb55314a733a05b726eee054adb8
thomas <psychoid@hotmail.fr>
2021-02-13 19:45:33 +0100

    test selenium notok

--- startup_pur_beurre/users/tests/test_selenium.py
+++ startup_pur_beurre/users/tests/test_selenium.py
@@ -3,16 +3,20 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(PATH, options=chrome_options)
 driver.maximize_window()
 driver.implicitly_wait(20)
 driver.get("http://localhost:8000/")
 print(driver.title)
 
-search = driver.find_element_by_id("search_product")
+ 
+search = driver.find_element_by_xpath("//form[@id='search_product_nav_bar']")
 search.send_keys("frosties")
 search.send_keys(Keys.RETURN)
 time.sleep(5)
