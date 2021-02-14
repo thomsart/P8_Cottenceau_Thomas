a68f690aa96d68cf12ccade88bc52b6d8c5cc848
thomas <psychoid@hotmail.fr>
2021-02-15 00:47:37 +0100

    test selenium 70% ok

--- startup_pur_beurre/users/tests/test_selenium.py
+++ startup_pur_beurre/users/tests/test_selenium.py
@@ -4,21 +4,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ex_co
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-chrome_options = Options()
-chrome_options.add_argument("--disable-extensions")
-driver = webdriver.Chrome(PATH, options=chrome_options)
+options = Options()
+options.add_argument("--disable-extensions")
+driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe", options=options)
 driver.maximize_window()
-driver.implicitly_wait(20)
+driver.implicitly_wait(10)
 driver.get("http://localhost:8000/")
-print(driver.title)
 
- 
-search = driver.find_element_by_xpath("//form[@id='search_product_nav_bar']")
+search = driver.find_element(By.ID, "id_product_name")
 search.send_keys("frosties")
-search.send_keys(Keys.RETURN)
+search.send_keys(Keys.ENTER)
+search.clear()
 time.sleep(5)
 
+try:
+    result = WebDriverWait(driver, 10).until(ex_co.presence_of_element_located((By)))
+except Exception:
+    print("erreur: La page ne s'est pas ouverte!!!")
+
+
 driver.quit()
+
+
+# with driver:
+#     wait = WebDriverWait(driver, 20)
+#     driver.get("http://localhost:8000/")
+#     driver.find_element(By.ID, "search_product_home").send_keys("frosties" + Keys.RETURN)
+#     first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+#     print(first_result.get_attribute("textContent"))
