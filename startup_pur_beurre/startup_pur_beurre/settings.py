a8e556b1920ce560afe87939ba14edfa198aa140
thomas <psychoid@hotmail.fr>
2021-02-17 09:35:22 +0100

    secret_key

--- startup_pur_beurre/startup_pur_beurre/settings.py
+++ startup_pur_beurre/startup_pur_beurre/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-spprd=2q3dmj97p8)5kufulxd$tj$*#t=_rp)r6#w!vmb5qo&'
+SECRET_KEY = os.environ['STARTUP_PUR_BEURRE_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
