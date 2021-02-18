0169e8afb330968e3ad005b53ab1cfc728818d2a
thomas <psychoid@hotmail.fr>
2021-02-18 19:00:21 +0100

    essai doublon

--- startup_pur_beurre/startup_pur_beurre/wsgi.py
+++ startup_pur_beurre/startup_pur_beurre/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startup_pur_beurre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startup_pur_beurre.settings.startup_pur_beurre.settings')
 
 application = get_wsgi_application()
