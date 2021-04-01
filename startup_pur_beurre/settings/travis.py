11166f5186a35bba9fa7a0cb1bc9ad01ebf5f53a
thomsart <psychoid@hotmail.fr>
2021-04-01 17:34:34 +0000

    modif .travis.yml

--- startup_pur_beurre/settings/travis.py
+++ startup_pur_beurre/settings/travis.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'KEY'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
+        'NAME': 'pur_beurre_db',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
