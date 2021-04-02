c967e0d75f61161d3cc245f171be58bce00f962d
thomsart <psychoid@hotmail.fr>
2021-04-02 08:18:13 +0000

    del 'json_folder' dans '_tools.py'

--- database/management/commands/_tools.py
+++ database/management/commands/_tools.py
@@ -4,7 +4,6 @@ import json
 import requests
 
 from database.models import Products
-from database.json_folder import *
 
 ################################################################################
 
