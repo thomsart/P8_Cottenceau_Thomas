f3282f1de2eca0bf52af7fda08b1e6c9d576c55a
thomsart <psychoid@hotmail.fr>
2021-04-06 12:34:15 +0000

    import Chrome

--- users/tests/test_selenium.py
+++ users/tests/test_selenium.py
@@ -5,7 +5,7 @@ import pytest
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
